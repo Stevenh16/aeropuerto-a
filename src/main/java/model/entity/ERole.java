6248dfcb4b7425e80da15062999ac97ac971b806
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-20 10:56:24 -0500

    [ADD]-> ERole AND Role

---
+++ src/main/java/model/entity/ERole.java
@@ -0,0 +1,5 @@
+package model.entity;
+
+public enum ERole {
+    ROLE_ADMIN, ROLE_CLIENT, ROLE_PASSENGER;
+}
---
+++ src/main/java/model/entity/Role.java
@@ -0,0 +1,21 @@
+package model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+}
