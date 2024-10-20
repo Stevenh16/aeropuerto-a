8495809d38557f8ea79ebcf41bb6956c99aa44a6
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-20 11:36:59 -0500

    [ADD]-> User,Role Repository

---
+++ src/main/java/model/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package model.repository;
+
+import model.entity.ERole;
+import model.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByRole(ERole name);
+}
