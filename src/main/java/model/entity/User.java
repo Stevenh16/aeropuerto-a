99a08e94ce8ee879d99adb04e1adcf051e9151d8
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-20 11:12:49 -0500

    [ADD] -> User (Super entity:Client,Passenger,Admin)

---
+++ src/main/java/model/entity/User.java
@@ -0,0 +1,39 @@
+package model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Builder
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="name")
+    private String name;
+    @Column(name="username")
+    private String username;
+    @Column(name="password")
+    private String password;
+    @Column(name="lastname")
+    private String lastname;
+    @Column(name="address")
+    private String address;
+    @Column(name="cell_phone")
+    private String cellphone;
+    @Column(name="email")
+    private String email;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
