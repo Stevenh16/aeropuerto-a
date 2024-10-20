9813ac97a52392cad6d630259f86c45ade40bbd8
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-20 13:32:15 -0500

    [MOD]-> Client,Passenger Repository (Add methods)

--- src/main/java/model/repository/PassengerRepository.java
+++ src/main/java/model/repository/PassengerRepository.java
@@ -3,5 +3,11 @@ package model.repository;
 import model.entity.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PassengerRepository extends JpaRepository<Passenger, Integer> {
+    Optional<Passenger> findByUsername(String username);
+    Optional<Passenger> findByEmail(String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
