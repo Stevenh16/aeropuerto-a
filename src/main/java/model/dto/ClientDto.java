55fd4bf7662805acbf0d147a91fd2b837c739fe9
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-20 11:42:53 -0500

    [MOD]-> Client, Passenger Dto (username, password)

--- src/main/java/model/dto/ClientDto.java
+++ src/main/java/model/dto/ClientDto.java
@@ -2,5 +2,5 @@ package model.dto;
 
 import java.util.List;
 
-public record ClientDto(int id, String name, String lastname, String address, String cellphone, String email, List<ReserveDto> reserves) {
+public record ClientDto(int id, String username, String password, String name, String lastname, String address, String cellphone, String email, List<ReserveDto> reserves) {
 }
