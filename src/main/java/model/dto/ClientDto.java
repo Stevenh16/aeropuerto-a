df9230d906a0d14934859a0e94813d516b491080
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-21 16:16:35 -0500

    [MOD]->(add roles)

--- src/main/java/model/dto/ClientDto.java
+++ src/main/java/model/dto/ClientDto.java
@@ -1,6 +1,9 @@
 package model.dto;
 
-import java.util.List;
+import model.entity.Role;
 
-public record ClientDto(int id, String username, String password, String name, String lastname, String address, String cellphone, String email, List<ReserveDto> reserves) {
+import java.util.List;
+import java.util.Set;
+
+public record ClientDto(int id, String username, String password, String name, String lastname, String address, String cellphone, String email, List<ReserveDto> reserves, Set<Role> roles) {
 }
--- src/main/java/model/dto/PassengerDto.java
+++ src/main/java/model/dto/PassengerDto.java
@@ -1,4 +1,8 @@
 package model.dto;
 
-public record PassengerDto(int id, String name, String username, String password, String lastname, String address, String cellphone, String email, ReserveDto reserve) {
+import model.entity.Role;
+
+import java.util.Set;
+
+public record PassengerDto(int id, String name, String username, String password, String lastname, String address, String cellphone, String email, ReserveDto reserve, Set<Role> roles) {
 }
