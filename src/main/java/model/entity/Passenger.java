dbb8e5af53d87d49e9e28d34273e46bde0ee558a
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-29 15:50:31 -0500

    [MOD]-> add atributes again

--- src/main/java/model/entity/Passenger.java
+++ src/main/java/model/entity/Passenger.java
@@ -10,7 +10,20 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name="passengers")
-public class Passenger extends User {
+public class Passenger{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="name")
+    private String name;
+    @Column(name="lastname")
+    private String lastname;
+    @Column(name="address")
+    private String address;
+    @Column(name="cell_phone")
+    private String cellphone;
+    @Column(name="email")
+    private String email;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="reserve_id")
     private Reserve reserve;
