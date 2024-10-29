b4a72a18ce50df1e722020b39919123571de5d4e
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-29 15:55:29 -0500

    [MOD]-> Delete methods

--- src/main/java/model/repository/ClientRepository.java
+++ src/main/java/model/repository/ClientRepository.java
@@ -3,11 +3,5 @@ package model.repository;
 import model.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ClientRepository extends JpaRepository<Client, Integer> {
-    Optional<Client> findByUsername(String username);
-    Optional<Client> findByEmail(String email);
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
 }
